701229c78e51f33702ddc35191b2294f9e84d7a0
kpninja <145045986+kpninja@users.noreply.github.com>
2023-10-13 23:47:20 -0400

    Update TestRunner.java
    changed feature path

--- Ds-algo/src/test/java/runner/TestRunner.java
+++ Ds-algo/src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src\\test\\java\\features\\Test01_ds_GetStarted.feature",
+@CucumberOptions(features="src\\test\\java\\features",
                   glue={"dsStepDefinitions","hooks"},
                   monochrome=true,
                   publish=true,
