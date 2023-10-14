70612e82c5f24a19d49b878fec39564e712ba0db
kpninja <145045986+kpninja@users.noreply.github.com>
2023-10-13 22:54:55 -0400

    Update TestRunner.java
    Test01_ds_GetStarted.feature updated

--- Ds-algo/src/test/java/runner/TestRunner.java
+++ Ds-algo/src/test/java/runner/TestRunner.java
@@ -6,7 +6,7 @@ import io.cucumber.junit.Cucumber;
 import io.cucumber.junit.CucumberOptions;
 
 @RunWith(Cucumber.class)
-@CucumberOptions(features="src\\test\\java\\features",
+@CucumberOptions(features="src\\test\\java\\features\\Test01_ds_GetStarted.feature",
                   glue={"dsStepDefinitions","hooks"},
                   monochrome=true,
                   publish=true,
